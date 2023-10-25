f3348d54e1f6b6d3ce68fdaef1d24ba04160ecde
krtatos <kkushagra626@gmail.com>
2023-10-26 01:58:40 +0530

    basic commit

---
+++ 11reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,23 @@
+import { createSlice ,nanoid } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    todos : [{ id: "1" , text: "halwwa" }]
+}
+
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{ 
+        addTodo:(state , action) => {
+            const todo={
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+
+        },
+        removeTodo: () => {},
+    }
+
+})
